f0c5ae0339f8de75a7eec4014a58172b51c2311a
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-05-31 18:24:26 +0200

    Add Software controller

---
+++ SefcomEnvironment/SefcomEnvironmentSite/Controllers/SoftwareController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SefcomEnvironmentSite.Controllers
+{
+    public class SoftwareController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
